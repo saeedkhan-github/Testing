a3c383be867a88a170ae685da21a779e0cd8f492
saeedullah321@gmail.com <saeedullah321@gmail.com>
2018-03-06 11:28:14 +0500

    Testing Code

---
+++ test/test.js
@@ -0,0 +1,11 @@
+const assert = require('chai').assert;
+
+const app=require('../app')
+
+describe('App',function(){
+
+    it('app should return Hello',function(){
+        assert.equal(app(),'hello');
+    });
+
+});
